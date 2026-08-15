008702afa7643012498eccae099e61c99d992db8
Peter Bierma <zintensitydev@gmail.com>
2026-08-15 10:27:56 -0400

    Add tests.

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2026-present Peter Bierma <zintensitydev@gmail.com>
-#
-# SPDX-License-Identifier: MIT
---
+++ tests/test_lazy.py
@@ -0,0 +1,37 @@
+from typing import Any
+from laziness import lazy, lazify
+from pytest import raises
+
+def _ignore(value: Any) -> None:
+    """
+    Resolve a lazy value through a namespace lookup and discard the result.
+    """
+    eval("value")
+
+def test_lazy_evaluation():
+    value = 0
+    def evaluate():
+        nonlocal value
+        value += 1
+
+    special = lazy("evaluate()")
+    assert value == 0
+    _ignore(special)
+    assert value == 1
+
+    special = lazify(evaluate)
+    assert value == 1
+    _ignore(special)
+
+def test_lazy_exceptions():
+    def kaboom():
+        raise ZeroDivisionError
+
+    special = lazy("kaboom()")
+
+    with raises(ZeroDivisionError):
+        _ignore(special)
+
+    special = lazify(kaboom)
+    with raises(ZeroDivisionError):
+        _ignore(special)
